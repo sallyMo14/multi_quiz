9a32b889d755ee7f40dea8440176b97c36f2483c
oalshokri <janataanfor@gmail.com>
2023-06-08 16:30:09 +0300

    - design multiple choice quiz page

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,3 +9,4 @@ const kL2 = Color(0xff5471ec);
 const kL22 = Color(0xff0bbbfe);
 const kL3 = Color(0xffa28dd0);
 const kL32 = Color(0xfff4bee7);
+const kBlueBg = Color(0xff3293f4);
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_quiz_s_t_tt9/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -11,6 +12,144 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    var questionNumber = 5;
+    var questionsCount = 10;
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              kBlueBg,
+              kL2,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 74, left: 24, right: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 44,
+                    width: 44,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                      style: ButtonStyle().copyWith(
+                          shape: MaterialStatePropertyAll(
+                            CircleBorder(),
+                          ),
+                          side: MaterialStatePropertyAll(
+                            BorderSide(color: Colors.white),
+                          ),
+                          padding: MaterialStatePropertyAll(EdgeInsets.all(8))),
+                    ),
+                  ),
+                  // OutlinedButton(
+                  //   onPressed: () {},
+                  //   style: ButtonStyle().copyWith(
+                  //     shape: MaterialStatePropertyAll(
+                  //       RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(25),
+                  //       ),
+                  //     ),
+                  //     side: MaterialStatePropertyAll(
+                  //       BorderSide(color: Colors.white),
+                  //     ),
+                  //   ),
+                  //   child: Row(
+                  //     children: [
+                  //       Icon(
+                  //         Icons.favorite,
+                  //         color: Colors.white,
+                  //       ),
+                  //       SizedBox(
+                  //         width: 8,
+                  //       ),
+                  //       const Text(
+                  //         '3',
+                  //         style: TextStyle(color: Colors.white),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                        height: 56,
+                        width: 56,
+                        child: CircularProgressIndicator(
+                          value: 0.7,
+                          color: Colors.white,
+                          backgroundColor: Colors.white12,
+                        ),
+                      ),
+                      Text(
+                        '05',
+                        style: TextStyle(
+                          fontFamily: 'Sf-Pro-Text',
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                    ),
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        side: BorderSide(color: Colors.white)),
+                  )
+                ],
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(34),
+                  child: Image.asset('assets/images/ballon-b.png'),
+                ),
+              ),
+              Text(
+                'question $questionNumber of $questionsCount',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'Sf-Pro-Text',
+                  color: Colors.white60,
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                'In Which City of Germany Is the Largest Port?',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontFamily: 'Sf-Pro-Text',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
