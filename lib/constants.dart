cab4b6c56f3173c0e4196b16f0abed8f9644d8e6
oalshokri <janataanfor@gmail.com>
2023-06-08 15:34:19 +0300

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kBlueIcon = Color(0xff0bbbfe);
+const kRedFont = Color(0xfff45e7a);
+const kGreyFont = Color(0xffb7adac);
+const kL1 = Color(0xffed6f9f);
+const kL12 = Color(0xffec8c6a);
+const kL2 = Color(0xff5471ec);
+const kL22 = Color(0xff0bbbfe);
+const kL3 = Color(0xffa28dd0);
+const kL32 = Color(0xfff4bee7);
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:multi_quiz_s_t_tt9/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
