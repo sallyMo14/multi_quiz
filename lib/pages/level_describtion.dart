3a5eb083a89bd205300ddc47214bd898e7dc0563
sallyMo14 <sallyabdalatti@gmail.com>
2023-06-25 15:03:08 +0300

    multiple choice screen

--- lib/pages/level_describtion.dart
+++ lib/pages/level_describtion.dart
@@ -89,7 +89,7 @@ class LevelDescription extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MultiQScreen(),
+                        builder: (context) => TrueFalseQuiz(),
                       ),
                     );
                   },
